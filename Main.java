357a862a2ceaf84f60f41d19569dbae8f0dc36ef
yzumite <101186651+ynukere@users.noreply.github.com>
2022-03-22 11:36:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int a=5;
+    char b='f';
+    double c=3.14;
+    byte d=8;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
